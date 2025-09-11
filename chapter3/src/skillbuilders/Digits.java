b5d45ef6ace64b5b8b49cb6fdffd445aba9f04ae
1100069235 <1100069235@L815WD220010.edu.cbe.ab.ca>
2025-09-11 10:08:30 -0600

    10:08 AM

---
+++ chapter3/src/skillbuilders/Digits.java
@@ -0,0 +1,16 @@
+package skillbuilders;
+
+public class Digits 
+{
+
+	public static void main(String[] args) 
+	{	
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- chapter3/src/skillbuilders/RectanglePerimeter.java
+++ chapter3/src/skillbuilders/RectanglePerimeter.java
@@ -1,13 +1,38 @@
 package skillbuilders;
 
+import java.util.Scanner;
+
 public class RectanglePerimeter 
 {
 
 	public static void main(String[] args) 
-	{
+	{	
+		// Declare Variables
+		int width;
+		int length;
+		
+		// Create scanner
+		Scanner input = new Scanner(System.in); 
+		
+		//Get width
+		System.out.print("Enter a width: ");
+		width = input.nextInt(); 
+		
+		// Get Length 
+		System.out.print("Enter a length: ");
+		length = input.nextInt(); 
 		
 		
+		System.out.println("Your width is:" + width);
+		System.out.println("Your length is:" + length);
 		
+		// Calculate perimeter
+        int perimeter = 2 * (length + width);
+        
+        // Show result
+        System.out.println("The total perimeter is: " + perimeter);
+        
+        input.close();
 		
 	}
 
