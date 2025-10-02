9ff52f18e69b3b8f22f649bb9def9d2685b766ef
1100069235 <1100069235@L815WD220010.edu.cbe.ab.ca>
2025-10-02 09:57:07 -0600

    9:57 AM

--- chapter4/src/skillbuilders/Hurricane.java
+++ chapter4/src/skillbuilders/Hurricane.java
@@ -9,8 +9,29 @@ public class Hurricane
 	{
 		Scanner input = new Scanner(System.in);
 		
+		System.out.print("Enter the hurricane category (1-5): ");
+		int category = input.nextInt();
 		
-		
+		 // Display wind speed info based on category
+		if (category == 1) 
+		{
+            System.out.println("Category 1: 74-95 mph, 64-82 kt, 119-153 km/hr");
+        } else if (category == 2) 
+        {
+            System.out.println("Category 2: 96-110 mph, 83-95 kt, 154-177 km/hr");
+        } else if (category == 3) 
+        {
+            System.out.println("Category 3: 111-130 mph, 96-113 kt, 178-209 km/hr");
+        } else if (category == 4) 
+        {
+            System.out.println("Category 4: 131-155 mph, 114-135 kt, 210-249 km/hr");
+        } else if (category == 5) 
+        {
+            System.out.println("Category 5: Greater than 155 mph, greater than 135 kt, greater than 249 km/hr");
+        } else 
+        {	
+            System.out.println("Invalid category. Please enter a number from 1 to 5.");
+        }
 		
 		
 		input.close();
---
+++ chapter4/src/skillbuilders/RandomNum.java
@@ -0,0 +1,20 @@
+package skillbuilders;
+
+import java.util.Scanner;
+
+public class RandomNum 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		
+		
+		input.close();
+		
+	}
+
+}
