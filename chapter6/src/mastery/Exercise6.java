eab6feca7f646c103d1600e4f9b7a14b9dcd0705
1100069235 <1100069235@L815WD220010.edu.cbe.ab.ca>
2025-11-14 10:45:49 -0700

    10:45 AM

---
+++ chapter6/src/mastery/Exercise6.java
@@ -0,0 +1,16 @@
+package mastery;
+
+import java.util.Scanner;
+
+public class Exercise6 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		
+		
+		input.close();
+	}
+
+}
