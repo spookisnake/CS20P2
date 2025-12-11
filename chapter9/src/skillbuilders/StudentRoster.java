cce69b5eea72c4eb1439c3e8a11308f420dd066a
1100069235 <1100069235@L815WD220010.edu.cbe.ab.ca>
2025-12-11 09:33:13 -0700

    9:33 AM

---
+++ chapter9/src/skillbuilders/StudentRoster.java
@@ -0,0 +1,35 @@
+package skillbuilders;
+
+import java.util.Scanner;
+
+public class StudentRoster 
+{
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		
+		int numStudents;
+		String[] students;
+		
+		System.out.print("How many students are there? ");
+		numStudents = input.nextInt();
+		input.nextLine();
+		
+		students = new String[numStudents];
+		
+		for(int i = 0; i < students.length; i++) 
+		{
+			System.out.print("Enter the name of student " + (i + 1) + ": ");
+			students[i] = input.nextLine();
+		}
+		
+		System.out.println(" ");
+		System.out.println("Student Roster");
+		
+		for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i]);
+        }
+
+	}
+
+}
