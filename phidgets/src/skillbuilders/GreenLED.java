2e5d64a2f993f8bdc09ab25efabb00d6962aba37
1100069235 <1100069235@L815WD220010.edu.cbe.ab.ca>
2025-12-18 09:21:33 -0700

    9:21 AM

--- phidgets/src/skillbuilders/GreenLED.java
+++ phidgets/src/skillbuilders/GreenLED.java
@@ -3,28 +3,28 @@ package skillbuilders;
 //Add Phidgets Library | You added a file called phidget22 when configuring your project. Import gives you access to the Phidgets library code inside that file. 
 import com.phidget22.*;
 
-public class GettingStarted {
+public class GreenLED {
   //Handle Exceptions | Exceptions will happen in your code from time to time. These are caused by unexpected things happening. Make sure you’ve added "throws Exception" to your main method.
   public static void main(String[] args) throws Exception{
 
       //Create | Here you've created a DigitalOutput object for your LED. An object represents how you interact with your device. DigitalOutput is a class from the Phidgets library that's used to provide a voltage to things like LEDs.
-      DigitalOutput redLED = new DigitalOutput();
+      DigitalOutput greenLED = new DigitalOutput();
 
       //Address | This tells your program where to find the device you want to work with. Your LED is connected to port 1 and your code reflects that. IsHubPortDevice must be set if you are not using a Smart Phidget (more on this later).
-      redLED.setHubPort(1);
-      redLED.setIsHubPortDevice(true);
+      greenLED.setHubPort(4);
+      greenLED.setIsHubPortDevice(true);
 
       //Open | Open establishes a connection between your object and your physical Phidget. You provide a timeout value of 1000 to give the program 1000 milliseconds (1 second) to locate your Phidget. If your Phidget can't be found, an exception will be thrown.
-      redLED.open(1000);
+      greenLED.open(1000);
 
       //Use your Phidgets | Here is where you can have some fun and use your Phidgets! You can turn your LED on/off by setting the state to true/false. The sleep command keeps your LED on by letting 1000 milliseconds pass before turning the LED off.
       while(true){
-          redLED.setState(true);
+    	  greenLED.setState(true);
           Thread.sleep(1000);
-          redLED.setState(false);
+          greenLED.setState(false);
           Thread.sleep(1000);
       }
+      //redLED
+      //greenLED
   }
 }
-
-
